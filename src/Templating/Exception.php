f45d6bf5465959469a34d4a804652550f7c4a26e
Stuart Wakefield <me@stuartwakefield.co.uk>
2014-08-23 18:16:55 +0100

    Added copyright notice Removing unnecessary directory level

--- src/Templating/Exception.php
+++ src/Templating/Exception.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @package Templating
+ * @author Stuart Wakefield <me@stuartwakefield.co.uk>
+ * @copyright Copyright (c) 2014 Stuart Wakefield (http://stuartwakefield.co.uk)
+ */
 namespace Templating;
 
 class Exception extends \Exception {}
